72485b92debc411ab8a07a5ce40c653a5aff0805
Hamit Mizrak <hamitmizrak@gmail.com>
2023-12-28 10:20:02 +0300

    javascript

---
+++ _5_Javascript/template.js
@@ -0,0 +1,72 @@
+// Single Comment JS
+/*Multiple Comment */
+// console.log('console.log');
+// console.info('console.info');
+// console.warn('console.warn');
+// console.error('console.error');
+
+// pop-up
+// alert('pop-up');
+// document.writeln('document.writeln');
+
+/////////////////////////////////////////////////
+// Variable (Global State)
+// var $_degiskenAdi44="Js öğreniyorum";
+// console.log($_degiskenAdi44);
+// var $_degiskenAdi44="Js öğreniyorum";
+// console.log($_degiskenAdi44);
+
+// // Hoisting
+// deneme="Kalem";
+// var deneme;
+// console.log(deneme);
+
+// // 2015 (ES6)
+// // LET
+// let data1="let data";
+// console.log(data1);
+// data1="let data44";
+// console.log(data1);
+
+// // CONST 
+// const data2="Js öğreniyorum";
+// console.log(data2);
+// data2="Js öğreniyorum";
+// console.log(data2);
+
+/////////////////////////////////////////////////
+// Operator Functions
+// Aritmetik Operatörler
+// + - * / %
+// &&=VE ||=VEYA !=değil
+// > >= < <= 
+// kullanıcıdan aldığım 2 tane sayıyı toplama yapan bir uygulama
+// let number1,number2,sum=0;
+//  number1=Number(prompt("Lütfen 1.sayıyı giriniz"));
+//  number2=Number(prompt("Lütfen 2.sayıyı giriniz"));
+//  sum=number1+number2;
+//  console.log("Toplama: "+sum);
+//  console.log(typeof prompt("Lütfen 1.sayıyı giriniz"));
+//  console.log(typeof Number(prompt("Lütfen 2.sayıyı giriniz")));
+//  console.log(typeof parseInt(prompt("Lütfen 2.sayıyı giriniz")));
+
+ /////////////////////////////////////////////////
+ // NaN
+ console.log(4/4);
+ console.log(4/"4");
+ console.log(4/"abc");
+
+ // Infinity 
+console.log(0/5);
+console.log(5/0);
+
+ // New  : Doğmak
+ // Null : Vefat 
+ // Zero : 0 
+ // isNaN :
+ console.log(isNaN(4/4));
+ console.log(!isNaN(4/4));
+ 
+ // Undefined 
+
+ // Escape Character
